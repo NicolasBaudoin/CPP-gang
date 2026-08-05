882148be9a6658c2c07f77e1af3b063052c9c83c
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-05 10:50:51 +0200

    Implement megaphone program to convert text to uppercase
    - started to do it for 1 arg, but saw it needed to include all argv
    - add "LOUD AND ..." sentence for no arg only
    - evolution : 
            - started with the use of secondary fonction ft_toupper
            -  started with while loop
            - then used for loop and ternary operation to implement the   upppercase conversion

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+    char **s = av; int j, i = 1;
+    
+    if (ac == 1)
+      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    
+    for (i = 1; s[i]; i++)
+        for (j = 0; s[i][j]; j++)
+            s[i][j] >= 'a' && s[i][j] <= 'z' ? std::cout << (s[i][j] -= 32) : std::cout << s[i][j];
+    std::cout << std::endl;
+    
+    return 0;
+}
