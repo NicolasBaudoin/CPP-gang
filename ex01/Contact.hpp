86f857d2bd1969cbe999f5a204cf65404d6c8f82
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-06 13:09:15 +0200

    Refactor Contact class access specifiers and methods
    Rearranged access specifiers and added a display method.

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -4,28 +4,29 @@
 # include <string>
 
 class Contact {
-  public:
-
-// Setters
-      void setFirstName(std::string value);
-      void setLastName(std::string value);
-      void setNickName(std::string value);
-      void setPhoneNumber(std::string value);
-      void setDarkestSecret(std::string value);
-
-// Getters
-      std::string getFirstName();
-      std::string getLastName();
-      std::string getNickName();
-      std::string getPhoneNumber();
-      std::string getDarkestSecret();
-
-  private:
+    private:
         std::string firstName;
         std::string lastName;
         std::string nickName;
         std::string phoneNumber;
         std::string darkestSecret;
+    public:
+
+    // Setters
+        void setFirstName(std::string value);
+        void setLastName(std::string value);
+        void setNickName(std::string value);
+        void setPhoneNumber(std::string value);
+        void setDarkestSecret(std::string value);
+    
+    // Getters
+        std::string getFirstName();
+        std::string getLastName();
+        std::string getNickName();
+        std::string getPhoneNumber();
+        std::string getDarkestSecret();
+        
+        void display();
 };
 
 #endif
