481fa6c6b6209ea90eb6fd628a419bea4157fe86
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-06 12:25:49 +0200

    Add getter methods to Contact class

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -5,11 +5,20 @@
 
 class Contact {
   public:
+
+// Setters
       void setFirstName(std::string value);
       void setLastName(std::string value);
       void setNickName(std::string value);
       void setPhoneNumber(std::string value);
       void setDarkestSecret(std::string value);
+
+// Getters
+      void getFirstName();
+      void getLastName();
+      void getNickName();
+      void getPhoneNumber();
+      void getDarkestSecret();
 
   private:
         std::string firstName;
