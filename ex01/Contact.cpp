2c25b50f4ad823f214a8ac5edaf59daf9edd3a77
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-06 11:50:46 +0200

    Add setter methods for Contact class

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -5,3 +5,16 @@
 void Contact::setFirstName(std::string value){
     firstName = value;
 }
+void Contact::setLastName(std::string value){
+    lastName = value;
+}
+void Contact::setNickName(std::string value){
+    nickName = value;
+}
+void Contact::setPhoneNumber(std::string value){
+    phoneNumber = value;
+}
+void Contact::setDarkestSecret(std::string value){
+    darkestSecret = value;
+}
+
