b8f20e463d0b77d91934459c79fe495b92ccaf27
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-06 11:48:56 +0200

    Add new fields and setters to Contact class

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,9 +6,17 @@
 class Contact {
   public:
       void setFirstName(std::string value);
+      void setLastName(std::string value);
+      void setNickName(std::string value);
+      void setPhoneNumber(std::string value);
+      void setDarkestSecret(std::string value);
 
   private:
         std::string firstName;
+        std::string lastName;
+        std::string nickName;
+        std::string phoneNumber;
+        std::string darkestSecret;
 };
 
 #endif
